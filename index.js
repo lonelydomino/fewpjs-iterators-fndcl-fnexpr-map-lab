8645373c900273946d2fab3fbc8efd417921a614
Michael Martinez <zeroeclipse@live.com>
2021-05-16 12:12:22 -0700

    commit tree f5351f7be9934f5867f5581d727aaffb8a5c4dab

--- index.js
+++ index.js
@@ -12,5 +12,22 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(makeBig);
 }
+
+function makeBig(string) {
+  const words = string.split(" ");
+  let newString = ""
+ for (let i = 0; i < words.length; i++) {
+   
+  newString += words[i].charAt(0).toUpperCase() + words[i].slice(1);
+  if (i !== words.length - 1) {
+    newString += " "
+  }
+}
+  return newString
+}
+
+// makeBig(titleCased()[0])
+
+// const array = titleCased().map(makeBig);
